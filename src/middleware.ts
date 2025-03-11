f9935fcd95d614d6c5878889122202a0daec9b92
Abhi Sharma <me.avish143@gmail.com>
2025-03-11 23:36:56 +0530

    /studio as protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/protected", "/namaste"]);
+const isProtectedRoute = createRouteMatcher(["/studio", "/namaste"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
