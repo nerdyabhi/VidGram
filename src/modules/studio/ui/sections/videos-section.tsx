1f5b9d7143bf2862ba92b05e7e459590965ef9d0
Abhi Sharma <me.avish143@gmail.com>
2025-03-14 19:49:23 +0530

    Infinite Scroll for studio videos

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,14 +1,90 @@
 "use client"
 
 import { trpc } from "@/trpc/client"
-
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
+import { InfiniteScroll } from "@/components/infinite-scroll"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import Link from "next/link"
 export const VideosSection = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+
+    return (
+        <Suspense fallback={<p>Loading something...</p>}>
+            <ErrorBoundary fallback={<p>Error</p>}>
+                <VideosSectionSuspense />
+            </ErrorBoundary>
+        </Suspense>
+    )
+}
+export const VideosSectionSuspense = () => {
+    const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
         { limit: 5 },
         { getNextPageParam: (lastPage) => lastPage.nextCursor });
+
     return (
+
         <div className="">
-            {JSON.stringify(data)}
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="pl-6 w-[510px]">Video</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Visibility</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Status</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Date</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Views</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Comments</TableHead>
+                            <TableHead className="pl-6 w-[510px]">Like</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {videos.pages.flatMap((page) => page.items).map((video) => (
+                            <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+                                <TableRow className="cursor-pointer text-center">
+                                    <TableCell>
+                                        {video.title}
+                                    </TableCell>
+
+                                    <TableCell>
+                                        visibility
+                                    </TableCell>
+
+                                    <TableCell>
+                                        status
+                                    </TableCell>
+
+                                    <TableCell>
+                                        {Date.now().toLocaleString()}
+                                    </TableCell>
+
+                                    <TableCell>
+                                        views
+                                    </TableCell>
+
+                                    <TableCell>
+                                        comments
+                                    </TableCell>
+
+                                    <TableCell>
+                                        Likes
+                                    </TableCell>
+
+
+                                </TableRow>
+                            </Link>
+                        ))}
+                    </TableBody>
+
+
+                </Table>
+            </div>
+            <div className="">
+                <InfiniteScroll
+                    hasNextPage={query.hasNextPage}
+                    isFetchingNextPage={query.isFetchingNextPage}
+                    fetchNextPage={query.fetchNextPage}
+                />
+            </div>
         </div>
     )
 }
--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -1,11 +1,15 @@
 import { VideosSection } from "../sections/videos-section"
 
 
-export const StudioView = ()=>{
+export const StudioView = () => {
 
     return (
-        <div className="">
-            <VideosSection/>
+        <div className="flex flex-col gap-y-6 pt-2.5">
+            <div className="px-4">
+                <h1 className="text-2xl font-bold">Channel Content</h1>
+                <p className="text-xs text-muted-foreground"> Manage Your channel content and videos </p>
+            </div>
+            <VideosSection />
         </div>
     )
 }
